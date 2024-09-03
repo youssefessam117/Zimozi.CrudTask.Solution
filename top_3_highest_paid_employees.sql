990a3b9ef206d7b1d2647b45c44b6131af70b063
youssefessam117 <youssefessam.w@gmail.com>
2024-09-03 20:45:37 +0300

    change

---
+++ top_3_highest_paid_employees.sql
@@ -0,0 +1,12 @@
+﻿SELECT EmployeeID, Name, Department, Salary 
+FROM (
+    SELECT EmployeeID, Name, Department, Salary,
+           row_number() OVER (PARTITION BY Dno ORDER BY Salary DESC) as rank
+    FROM Employee
+) ranked
+WHERE rank <= 3;
+
+----------------------
+-- i used row_number inside a sub queru 
+-- row_number gave me resul with out any duplicate 
+-- and i get only the first 3 by where <= 3 
